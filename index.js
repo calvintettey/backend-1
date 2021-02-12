d00d10c3df3b7ceb6873e8dd0f72ad152c448785
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-02-12 14:32:26 +0000

    added idek

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-const http = require("http");
+// const http = require("http");
+const express = require("express");
 
 const handleAllRequests = (requestObject, responseObject) => {
   const url = requestObject.url;
   responseObject.setHeader("content-type", "text/html");
   if (url === "/signup") {
-    responseObject.write("<h2>Signup Here</h2>");
-    responseObject.end();
+    responseObject.send("<h2>Signup Here</h2>");
   } else if (url === "/login") {
-    responseObject.write("<h2>Login Page</h2>");
-    responseObject.end();
+    responseObject.send("<h2>Login Page</h2>");
   } else if (url === "/photos") {
-    responseObject.write("<h2>Gallery</h2>");
-    responseObject.end();
+    responseObject.send("<h2>Gallery</h2>");
   } else if (url === "/books") {
-    responseObject.write("<h2>Library</h2>");
-    responseObject.end();
+    responseObject.send("<h2>Library</h2>");
   } else if (url === "/") {
-    responseObject.write("<h1>Home Page</h1>");
-    responseObject.end();
+    responseObject.send("<h1>Home Page</h1>");
   } else {
-    responseObject.write("404 page does not exist");
-    responseObject.end();
+    responseObject.send("404 page does not exist");
   }
 };
 
-const server = http.createServer(handleAllRequests);
+const handleGetSignup = (req, res) => {
+  res.send("<h1>This is Sign Up Get</h1>");
+};
+
+const handlePutSignup = (req, res) => {
+  res.send("<h1>This is Sign Up Put</h1>");
+};
+
+const handleDeleteSignup = (req, res) => {
+  res.send("<h1>This is Sign Up Delete</h1>");
+};
+
+const handlePostSignup = (req, res) => {
+  res.send("<h1>This is Sign Up Post</h1>");
+};
+
+// const server = http.createServer(handleAllRequests);
+const server = express();
+
+// server.use(handleAllRequests);
+
+server.get("/signup", handleGetSignup);
+server.put("/signup", handlePutSignup);
+server.delete("/signup", handleDeleteSignup);
+server.post("/signup", handlePostSignup);
 
 server.listen(3000, "127.0.0.1", () => {
   console.log("it's working");
